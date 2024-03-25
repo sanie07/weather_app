5670153057b03ce2e1c6d0d780c2d94b65c8cbf1
sanie07 <sanie.peralta@fiuna.edu.py>
2024-03-24 21:41:52 -0400

    Added search functionnality using the API

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
+function UpadateWeatherData(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "ed03b85af3afdd1609b1odt4bf2f3bbf";
+  let unit = "metric";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${unit}`;
+  axios.get(apiUrl).then(UpadateWeatherData);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchElement = document.querySelector("#search-form");
 searchElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Asuncion");
