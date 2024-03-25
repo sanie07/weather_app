4b852e5374c93fd0a87ffc7255ebeac4027eaef7
sanie07 <sanie.peralta@fiuna.edu.py>
2024-03-24 20:27:32 -0400

    Added basic search engine

---
+++ src/app.js
@@ -0,0 +1,9 @@
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let searchElement = document.querySelector("#search-form");
+searchElement.addEventListener("submit", handleSearchSubmit);
